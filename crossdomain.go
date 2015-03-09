f1629ade2a848eaadf9b51be7c051c1a247d079d
Mat Evans <mat@crzy.co.uk>
2015-03-09 07:44:20 +0000

    Initial example

---
+++ crossdomain.go
@@ -0,0 +1,21 @@
+package crossdomain
+
+import (
+  "fmt"
+  "net/http"
+  )
+  
+// make a struct for our http handler
+type CDHandler struct {}
+
+// use to build the xml
+type crossdomain struct {
+}
+
+func (*c CDHandler) serveHTTP(w http.ResponseWriter, r *http.Request) {
+
+  // serve the relevant crossdomain.xml stuff here
+  // simple to begin
+  fmt.Sprintf("<?xml version="1.0" ?> \n <cross-domain-policy> \n <allow-access-from domain="*" /> \n </cross-domain-policy>"
+)
+}
